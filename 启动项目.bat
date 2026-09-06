6b4543bb83e3c392143179e0b10b252e2272c322
Hechrios <25563129@qq.com>
2026-09-06 13:06:58 +0800

    修复了快捷启动方式

--- 启动项目.bat
+++ 启动项目.bat
@@ -3,5 +3,5 @@ cd /d "%~dp0"
 echo Starting SC-TW Converter...
 start /min "SC-TW Server" cmd /c "python -m http.server 8000 --bind 127.0.0.1"
 timeout /t 2 /nobreak >nul
-start "" "http://127.0.0.1:8000/index.html?v=5"
+start "" "http://127.0.0.1:8000/index.html?v=7"
 exit
